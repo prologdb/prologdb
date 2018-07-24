1a1c659477079b3ded95c019441a6f14409118da
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-07-24 20:21:19 +0200

    Added ExecutionPlanner and non-optimizing implementation

---
+++ core/src/main/kotlin/com/github/prologdb/execplan/planner/ExecutionPlanner.kt
@@ -0,0 +1,12 @@
+package com.github.prologdb.execplan.planner
+
+import com.github.prologdb.PrologDatabase
+import com.github.prologdb.execplan.PlanStep
+import com.github.prologdb.runtime.query.Query
+
+interface ExecutionPlanner {
+    /**
+     * Plans the execution of the given query against the given database.
+     */
+    fun planExecution(query: Query, database: PrologDatabase): PlanStep
+}
---
+++ core/src/main/kotlin/com/github/prologdb/execplan/planner/NoOptimizationExecutionPlanner.kt
@@ -0,0 +1,23 @@
+package com.github.prologdb.execplan.planner
+
+import com.github.prologdb.PrologDatabase
+import com.github.prologdb.execplan.*
+import com.github.prologdb.runtime.query.AndQuery
+import com.github.prologdb.runtime.query.OrQuery
+import com.github.prologdb.runtime.query.PredicateQuery
+import com.github.prologdb.runtime.query.Query
+
+/**
+ * An execution planner that does no optimizations at all; behaves exactly like a
+ * classic prolog system, no indices are utilized.
+ */
+class NoOptimizationExecutionPlanner : ExecutionPlanner {
+    override fun planExecution(query: Query, database: PrologDatabase): PlanStep {
+        return when(query) {
+            is OrQuery -> UnionStep(query.goals.map { this.planExecution(it, database)})
+            is AndQuery -> JoinStep(query.goals.map { this.planExecution(it, database) })
+            is PredicateQuery -> ProveStep(query.predicate)
+            else -> throw PrologQueryException("Unsupported query type ${query::class.simpleName}")
+        }
+    }
+}
