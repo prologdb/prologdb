6aeaf731cd1c9304b7fcf83c63440fe5eb85c139
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-07-24 20:19:43 +0200

    Cleaned up ProveStep after all the refactorings

--- core/src/main/kotlin/com/github/prologdb/execplan/ProveStep.kt
+++ core/src/main/kotlin/com/github/prologdb/execplan/ProveStep.kt
@@ -2,6 +2,7 @@ package com.github.prologdb.execplan
 
 import com.github.prologdb.PrologDatabase
 import com.github.prologdb.runtime.RandomVariableScope
+import com.github.prologdb.runtime.knowledge.library.PredicateIndicator
 import com.github.prologdb.runtime.lazysequence.LazySequence
 import com.github.prologdb.runtime.lazysequence.filterRemainingNotNull
 import com.github.prologdb.runtime.lazysequence.mapRemaining
@@ -13,15 +14,18 @@ import com.github.prologdb.runtime.unification.VariableBucket
  * Most simple plan: scan all known predicates of a given [PredicateIndicator]
  */
 class ProveStep(
-    val unifyWith: Predicate
+    val goal: Predicate
 ) : PlanStep {
-    override fun execute(db: PrologDatabase, randomVarsScope: RandomVariableScope, variables: VariableBucket): LazySequence<Unification> {
-        val predicateStore = db.predicateStores[unifyWith] ?: return LazySequence.empty()
 
-        return predicateStore.all().toLazySequence(predicateStore)
-                .mapRemaining { it.unify(unifyWith, randomVarsScope) }
+    private val goalIndicator = PredicateIndicator.of(goal)
+
+    override fun execute(db: PrologDatabase, randomVarsScope: RandomVariableScope, variables: VariableBucket): LazySequence<Unification> {
+        val predicateStore = db.predicateStores[goalIndicator] ?: return LazySequence.empty()
+
+        return predicateStore.all()
+                .mapRemaining { it.second.unify(goal, randomVarsScope) }
                 .filterRemainingNotNull()
     }
 
-    override val explanation by lazy({ Predicate("prove", arrayOf(unifyWith)) })
+    override val explanation by lazy { Predicate("prove", arrayOf(goal)) }
 }
