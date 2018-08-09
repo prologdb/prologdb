88000a42589f2f29a6edbca95f6b26542a54cbeb
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-08-09 16:40:35 +0200

    WIP HeapFile: added HeapManager

--- core/src/main/kotlin/com/github/prologdb/util/memory/HeapManager.kt
+++ core/src/main/kotlin/com/github/prologdb/util/memory/HeapManager.kt
@@ -2,6 +2,14 @@ package com.github.prologdb.util.memory
 
 import java.util.SortedSet
 import java.util.TreeSet
+import kotlin.collections.ArrayList
+import kotlin.collections.any
+import kotlin.collections.first
+import kotlin.collections.firstOrNull
+import kotlin.collections.forEach
+import kotlin.collections.lastIndex
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * Manager for a heap data structure. Is not associated to the actual resources; is
@@ -46,24 +54,160 @@ class FirstFitHeapManager(
      * If, however, the smallest fitting chunk is of size 8192, it would be split into one of size 1752 and
      * one of 6440.
      */
-    val minimumViableChunkSize: Long
+    val minimumViableChunkSize: Long,
+
+    /**
+     * When [allocate] cannot find a large enough chunk it will call [defragmentFreeSpace] with this value as the
+     * `effortFactor` parameter and attempt another try.
+     * Must be in the range [0; 1]
+     */
+    private val defragEffortFactor: Float = 0.1f
 ) : HeapManager {
     override var size: Long = initialSize
         private set
 
+    /**
+     * The free chunks, sorted by size ascending
+     */
     private val freeChunksSorted: SortedSet<LongRange> = TreeSet<LongRange>(compareBy(LongRange::size))
 
+    /**
+     * Amount of free space (essentially `freeChunksSorted.sumBy { it::size }`, but maintained
+     * separately for speed when accessing).
+     */
+    var freeSpaceAmount: Long = size
+        private set
+
     init {
-        freeChunksSorted.add(0L .. size)
+        if (defragEffortFactor < 0f || defragEffortFactor > 1f) {
+            throw IllegalArgumentException("Defragmentation effort factor must be in [0; 1]")
+        }
+        freeChunksSorted.add(0L .. (size - 1))
+    }
+
+    /**
+     * Defragments the freed chunk list (search for separate but adjacent free chunks and combine them into one).
+     * @param effortFactor In `[0; 1]`. Fraction of the number of chunks to look at. E.g. if there are 46 free chunks and this
+     *                     is set to `0.2f`, will look at a maximum of `ceil(46 * 0.2 = 9.2) = 10` chunks.
+     * @param abortIfCombinedOneOfAtLeast The defragmentation will be aborted as soon as a chunk of at least this
+     *                     size was the result of a combination of smaller chunks. If null does not abort.
+     * @return Whether fragmentation of the free chunk list was reduced as a result of the call.
+     */
+    fun defragmentFreeSpace(effortFactor: Float, abortIfCombinedOneOfAtLeast: Long? = null): Boolean {
+        if (defragEffortFactor < 0f || defragEffortFactor > 1f) {
+            throw IllegalArgumentException("Effort factor must be in [0; 1]")
+        }
+        if (abortIfCombinedOneOfAtLeast != null && abortIfCombinedOneOfAtLeast < 1) {
+            throw IllegalArgumentException("abortIfCombinedOneOfAtLeast must be greater than 0.")
+        }
+
+        if (freeChunksSorted.size < 2) return false
+
+        val maxNChunksToLookAt = Math.ceil((freeChunksSorted.size * effortFactor).toDouble()).toInt()
+        var nChunksLookedAt = 0
+        val chunksRandomAccess = ArrayList(freeChunksSorted)
+        val toRemove = ArrayList<LongRange>(freeChunksSorted.size / 2)
+        val toAdd = ArrayList<LongRange>(freeChunksSorted.size / 2)
+        var pivotPointer = 0 // index of the next pivot within chunksRandomAccess
+        var atLeastOneCombined = false // for the return value
+
+        pivotCandidates@while (nChunksLookedAt < maxNChunksToLookAt) {
+            val pivot = chunksRandomAccess[pivotPointer]
+            nChunksLookedAt++
+            findForPivot@for (nextIndex in 0..chunksRandomAccess.lastIndex) {
+                if (nChunksLookedAt > maxNChunksToLookAt) break
+
+                if (nextIndex == pivotPointer) continue
+
+                val next = chunksRandomAccess[nextIndex]
+                nChunksLookedAt++
+
+                if (pivot.last + 1 == next.first || next.last + 1 == pivot.first) {
+                    chunksRandomAccess.remove(pivot)
+                    chunksRandomAccess.remove(next)
+                    toRemove.add(pivot)
+                    toRemove.add(next)
+
+                    val combined = min(pivot.first, next.first)..max(pivot.last, next.last)
+                    chunksRandomAccess.add(combined)
+                    toAdd.add(combined)
+
+                    atLeastOneCombined = true
+
+                    if (abortIfCombinedOneOfAtLeast != null && combined.size >= abortIfCombinedOneOfAtLeast) {
+                        break@pivotCandidates
+                    }
+
+                    pivotPointer = 0
+                    break@findForPivot
+                }
+            }
+            pivotPointer++
+        }
+
+        toRemove.forEach { freeChunksSorted.remove(it) }
+        toAdd.forEach { freeChunksSorted.add(it) }
+
+        return atLeastOneCombined
     }
 
     override fun allocate(desiredSize: Long, enlargeIfNecessary: Boolean): LongRange? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (freeSpaceAmount < desiredSize) {
+            if (!enlargeIfNecessary) return null // there is nothing we can do, out of memory
+            val newChunk = size..(size + desiredSize - 1)
+            size += desiredSize
+            return newChunk
+        }
+
+        val chunk = freeChunksSorted.firstOrNull { it.size >= desiredSize }
+            // defrag is it is likely to help
+            ?: if (defragEffortFactor > 0f && desiredSize / defragEffortFactor <= freeSpaceAmount) {
+                    if (defragmentFreeSpace(defragEffortFactor, desiredSize)) {
+                        freeChunksSorted.first { it.size >= desiredSize }
+                    } else null
+                } else null
+            ?: if (enlargeIfNecessary) {
+                    val newChunk = size..(size + desiredSize - 1)
+                    size += desiredSize
+                    newChunk
+                } else null
+
+        chunk ?: return null
+
+        freeChunksSorted.remove(chunk)
+
+        val chunkOversize = chunk.size - desiredSize
+        val allocated: LongRange
+        if (chunkOversize > minimumViableChunkSize) {
+            allocated = chunk.first..(chunk.first + desiredSize - 1)
+            val remainder = (allocated.last + 1)..chunk.last
+            freeChunksSorted.add(remainder)
+        }
+        else {
+            allocated = chunk
+        }
+        freeSpaceAmount -= allocated.size
+        return allocated
     }
 
     override fun free(chunk: LongRange) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (!chunk.isAscending) throw IllegalArgumentException("Chunk must be ascending.")
+        if (chunk.start < 0 || chunk.last < 0) throw IllegalArgumentException("Chunk start and end must be positive.")
+        if (chunk.last > size) throw IllegalArgumentException("Cannot free space outside of this heap.")
+
+        if (freeChunksSorted.any { it.overlapsWith(chunk) }) {
+            throw IllegalArgumentException("The given range was not entirely occupied prior to this call.")
+        }
+
+        freeChunksSorted.add(chunk)
+        freeSpaceAmount += chunk.size
     }
+}
+
+private infix fun LongRange.overlapsWith(other: LongRange): Boolean {
+    // thanks to https://stackoverflow.com/questions/3269434/whats-the-most-efficient-way-to-test-two-integer-ranges-for-overlap/3269471
+
+    return this.first <= other.last && other.first <= this.last
 }
 
 private val LongRange.isAscending
---
+++ core/src/test/kotlin/com.github.prologdb/util/memory/FirstFitHeapManagerTest.kt
@@ -0,0 +1,129 @@
+package com.github.prologdb.util.memory
+
+import io.kotlintest.matchers.beGreaterThanOrEqualTo
+import io.kotlintest.matchers.should
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.FreeSpec
+
+class FirstFitHeapManagerTest : FreeSpec({
+    "allocate" - {
+        "exact size when minimum viable = 1" {
+            val heap = FirstFitHeapManager(1024, 1, 1f)
+
+            val allocated = heap.allocate(1000, false)
+
+            allocated!!
+            allocated.last - allocated.first shouldBe 999L
+            heap.freeSpaceAmount shouldBe 24L
+        }
+
+        "allocate all space exact match" {
+            val heap = FirstFitHeapManager(1024, 1, 1f)
+
+            val allocatedA = heap.allocate(24, false)
+            val allocatedB = heap.allocate(1000, false)
+
+            allocatedA!!
+            allocatedB!!
+            allocatedA.last - allocatedA.first shouldBe 23L
+            allocatedB.last - allocatedB.first shouldBe 999L
+            heap.freeSpaceAmount shouldBe 0L
+            heap.allocate(1, false) shouldBe null
+        }
+
+        "splits of at least minimum viable size" {
+            val heap = FirstFitHeapManager(1024, 100, 1f)
+
+            val allocatedA = heap.allocate(950, false)
+            allocatedA!!
+            heap.freeSpaceAmount shouldBe 0L
+            heap.allocate(50, false) shouldBe null
+        }
+
+        "not enlarged if not allowed" {
+            val heap = FirstFitHeapManager(1024, 100, 1f)
+
+            heap.allocate(512, false)
+            heap.size shouldBe 1024L
+            heap.allocate(1024, false) shouldBe null
+            heap.size shouldBe 1024L
+            heap.freeSpaceAmount shouldBe 512L
+        }
+
+        "not enlarged if not necessary but allowed" {
+            val heap = FirstFitHeapManager(1024, 10, 1f)
+
+            val allocated = heap.allocate(1000, true)
+
+            allocated!!
+            heap.size shouldBe 1024L
+            heap.freeSpaceAmount shouldBe 24L
+        }
+
+        "enlarged if necessary and allowed" {
+            val heap = FirstFitHeapManager(1024, 1, 1f)
+
+            val allocated = heap.allocate(2048, true)
+
+            allocated!!
+            allocated.last - allocated.first shouldBe 2047L
+            heap.size should beGreaterThanOrEqualTo(2048L)
+        }
+    }
+
+    "free" - {
+        "space available after free" {
+            val heap = FirstFitHeapManager(1024, 1, 0f)
+
+            val allocated = heap.allocate(900, false)
+            allocated!!
+            heap.freeSpaceAmount shouldBe (1024L - 900L)
+            heap.free(allocated)
+            heap.freeSpaceAmount shouldBe 1024L
+        }
+
+        "without defragmentation" - {
+            "not enough space without defrag - not allocated" {
+                val heap = FirstFitHeapManager(1024, 1, 0f)
+
+                val allocated = heap.allocate(768L, false)
+                allocated!!
+                heap.free(allocated)
+
+                heap.allocate(980, false) shouldBe null
+            }
+
+            "not enough space without defrag - enlarged" {
+                val heap = FirstFitHeapManager(1024, 1, 0f)
+
+                val allocatedA = heap.allocate(768L, false)
+                allocatedA!!
+                heap.free(allocatedA)
+
+                val allocatedB = heap.allocate(980, true)
+                allocatedB!!
+                allocatedB.last - allocatedB.first shouldBe 979L
+                heap.size should beGreaterThanOrEqualTo(768L + 980L)
+            }
+        }
+
+        "with defragmentation" - {
+            "enough space only with defrag" {
+                val heap = FirstFitHeapManager(1024, 128, 1f)
+
+                val allocatedA = heap.allocate(980, false)
+                allocatedA!!
+                heap.free(allocatedA)
+                val allocatedB = heap.allocate(512, false)
+                val allocatedC = heap.allocate(512, false)
+
+                allocatedB!!
+                allocatedC!!
+                allocatedB.last - allocatedB.first shouldBe 511L
+                allocatedC.last - allocatedC.first shouldBe 511L
+                heap.size shouldBe 1024L
+                heap.freeSpaceAmount shouldBe 0L
+            }
+        }
+    }
+})
