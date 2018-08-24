43eb4d57f2f19267aee82a954cc0d9ef64298d50
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-08-24 15:33:53 +0200

    Added test for InMemoryMetadataRepository

---
+++ core/src/test/kotlin/com.github.prologdb/util/metadata/InMemoryMetadataRepositoryTest.kt
@@ -0,0 +1,30 @@
+package com.github.prologdb.util.metadata
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldEqual
+import io.kotlintest.matchers.shouldThrow
+import io.kotlintest.specs.FreeSpec
+
+class InMemoryMetadataRepositoryTest : FreeSpec({
+    "store and retrieve" {
+        val repo = InMemoryMetadataRepository()
+        repo.save("Foo", "Bar")
+
+        repo.load("Foo", String::class) shouldEqual "Bar"
+    }
+
+    "store and load with wrong type" {
+        val repo = InMemoryMetadataRepository()
+        repo.save("foo", "bar")
+
+        shouldThrow<RuntimeException> {
+            repo.load("foo", Long::class)
+        }
+    }
+
+    "load non existing" {
+        val repo = InMemoryMetadataRepository()
+
+        repo.load("key", String::class) shouldBe null
+    }
+})
