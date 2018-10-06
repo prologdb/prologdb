b502fe0754d335280bda7713602b16800906c4e8
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-10-06 22:53:04 +0200

    Install protoc on travis

--- core/src/main/kotlin/com/github/prologdb/dbms/PrologDatabaseManager.kt
+++ core/src/main/kotlin/com/github/prologdb/dbms/PrologDatabaseManager.kt
@@ -1,10 +1,20 @@
 package com.github.prologdb.dbms
 
+import com.github.prologdb.execplan.planner.ExecutionPlanner
+
 /**
  * The central element of the database management system (compare RDBMS). For every
  * prolog database a database server is managing there is one instance of this class.
  * For interactions with the database, obtain a [PrologDatabaseView].
  */
 class PrologDatabaseManager(
+    @Volatile var planner: ExecutionPlanner
+) {
 
-)
+}
+
+class PrologDatabaseWorker(
+    private val view: PrologDatabaseView
+) {
+
+}
