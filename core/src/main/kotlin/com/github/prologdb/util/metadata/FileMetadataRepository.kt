37d2ce958a7e579c7f2d7d039f0c7cccfe1c864a
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-11-08 17:29:36 +0100

    Added FileMetadataRepository

---
+++ core/src/main/kotlin/com/github/prologdb/util/metadata/FileMetadataRepository.kt
@@ -0,0 +1,39 @@
+package com.github.prologdb.util.metadata
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.nio.file.Path
+import java.util.*
+
+/**
+ * Writes the data to a file
+ */
+class FileMetadataRepository(
+    file: Path,
+    private val valueObjectMapper: ObjectMapper
+) : MetadataRepository {
+
+    private val file = file.toFile()
+
+    private val properties: Properties by lazy {
+        val fromFile = Properties()
+        FileInputStream(this@FileMetadataRepository.file).use { fromFile.load(it) }
+        fromFile
+    }
+
+    override fun save(key: String, value: Any) {
+        bulkSave(mapOf(key to value))
+    }
+
+    override fun bulkSave(data: Map<String, Any>) {
+        for ((key, value) in data) {
+            properties.setProperty(key, valueObjectMapper.writeValueAsString(value))
+        }
+        FileOutputStream(file).use { properties.store(it, "last modified properties: ${data.keys.joinToString()}") }
+    }
+
+    override fun <T : Any> load(key: String, valueClass: Class<T>): T? {
+        return valueObjectMapper.readValue(properties.getProperty(key, "null"), valueClass)
+    }
+}
