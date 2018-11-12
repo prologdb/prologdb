c7d29309bae4155eaf238aec7dc25b021f95d2fd
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-11-12 17:49:55 +0100

    orch: drafted global directives

--- orchestrated/src/main/kotlin/com/github/prologdb/orchestration/engine/PrologDBEngine.kt
+++ orchestrated/src/main/kotlin/com/github/prologdb/orchestration/engine/PrologDBEngine.kt
@@ -1,11 +1,15 @@
 package com.github.prologdb.orchestration.engine
 
 import com.github.prologdb.async.LazySequence
+import com.github.prologdb.dbms.PrologDatabaseManager
+import com.github.prologdb.execplan.planner.NoOptimizationExecutionPlanner
 import com.github.prologdb.net.session.DatabaseEngine
 import com.github.prologdb.orchestration.SessionContext
 import com.github.prologdb.runtime.PrologRuntimeException
+import com.github.prologdb.runtime.knowledge.library.ClauseIndicator
 import com.github.prologdb.runtime.query.Query
 import com.github.prologdb.runtime.term.Predicate
+import com.github.prologdb.runtime.term.PrologString
 import com.github.prologdb.runtime.unification.Unification
 import org.slf4j.LoggerFactory
 import java.nio.file.Path
@@ -14,7 +18,7 @@ import java.util.concurrent.ConcurrentHashMap
 internal val log = LoggerFactory.getLogger("prologdb.engine")
 
 class PrologDBEngine(
-    private val dataDirectory: Path
+    dataDirectory: Path
 ) : DatabaseEngine<SessionContext> {
 
     private val dirManager = ServerDataDirectoryManager(dataDirectory)
@@ -29,7 +33,13 @@ class PrologDBEngine(
         val kbNames = dirManager.serverMetadata.load("knowledgeBases", Array<String>::class.java) ?: emptyArray()
         for (kbName in kbNames) {
             log.info("Initializing knowledge base $kbName")
-            knowledgeBases[kbName] = TODO()
+            knowledgeBases[kbName] = DatabaseManagerKnowledgeBase(
+                PrologDatabaseManager(
+                    dirManager.directoryForKnowledgeBase(kbName),
+                    NoOptimizationExecutionPlanner()
+                )
+            )
+            log.info("Knowledge base $kbName initialized.")
         }
     }
 
@@ -45,6 +55,11 @@ class PrologDBEngine(
     }
 
     override fun startDirective(session: SessionContext, command: Predicate, totalLimit: Long?): LazySequence<Unification> {
+        val indicator = ClauseIndicator.of(command)
+        if (globalDirectives.supportsDirective(indicator)) {
+            return globalDirectives.startDirective(session, command, totalLimit)
+        }
+
         val kb = session.knowledgeBase?.second ?: return lazyError(PrologRuntimeException("No knowledge base selected."))
         return kb.startDirective(session, command, totalLimit)
     }
@@ -53,7 +68,18 @@ class PrologDBEngine(
         TODO()
 
         // TODO: wait for all data to be written to disk
-        // TODO: write metadata
+    }
+
+    private val globalDirectives = ProgramaticServerKnowledgeBase {
+        directive("select_knowledge_base"/1) { ctxt, args ->
+            val name = (args[0] as? PrologString)?.toKotlinString()
+                ?: return@directive lazyError<Unification>(PrologRuntimeException("Argument 1 to select_knowledge_base/1 must be a string, got ${args[0].prologTypeName}"))
+
+            val base = knowledgeBases[name]
+                ?: return@directive lazyError<Unification>(PrologRuntimeException("Knowledge base $name does not exist."))
+
+            ctxt.knowledgeBase = Pair(name, base)
+            LazySequence.of(Unification.TRUE)
+        }
     }
 }
-
