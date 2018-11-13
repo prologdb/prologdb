402c4c6d425c555ed112b13971df1684a9745b36
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-11-13 16:44:16 +0100

    intf: handle consumption requests for closed queries

--- socket-interface/src/main/kotlin/com/github/prologdb/net/ServerInterface.kt
+++ socket-interface/src/main/kotlin/com/github/prologdb/net/ServerInterface.kt
@@ -147,6 +147,15 @@ class ServerInterface(
             return
         }
 
+        if (context.closed) {
+            handle.queueMessage(QueryRelatedError(
+                command.queryId,
+                QueryRelatedError.Kind.ERROR_GENERIC,
+                "Query ${command.queryId} already closed - cannot consume more solutions"
+            ))
+            return
+        }
+
         context.doWith {
             it.registerConsumptionRequest(command)
         }
