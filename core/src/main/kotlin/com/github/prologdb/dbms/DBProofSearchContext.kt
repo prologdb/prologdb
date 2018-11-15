cb96feb7362f3e589adb366a49cdbfe818c12e9f
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-11-15 12:43:19 +0100

    core: added DBProofSearchContext#staticBuitlins

--- core/src/main/kotlin/com/github/prologdb/dbms/DBProofSearchContext.kt
+++ core/src/main/kotlin/com/github/prologdb/dbms/DBProofSearchContext.kt
@@ -1,11 +1,26 @@
 package com.github.prologdb.dbms
 
+import com.github.prologdb.runtime.builtin.NativeCodeRule
 import com.github.prologdb.runtime.knowledge.ProofSearchContext
 import com.github.prologdb.runtime.knowledge.Rule
 import com.github.prologdb.runtime.knowledge.library.ClauseIndicator
 import com.github.prologdb.storage.predicate.PredicateStore
 
 interface DBProofSearchContext : ProofSearchContext {
+    /**
+     * Storages for dynamic facts
+     */
     val predicateStores: Map<ClauseIndicator, PredicateStore>
+
+    /**
+     * Definitions of dynamic rules
+     */
     val rules: Map<ClauseIndicator, List<Rule>>
+
+    /**
+     * Builtins. Those cannot be modified and calls to these indicators
+     * are never handled by the dynamic clauses in [predicateStores]
+     * and [rules].
+     */
+    val staticBuiltins: Map<ClauseIndicator, NativeCodeRule>
 }
