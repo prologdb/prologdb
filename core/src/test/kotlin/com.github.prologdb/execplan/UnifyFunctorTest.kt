937b8447470f911e1ecb0b1b4b5ce5a68f07a98c
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-12-21 15:06:26 +0100

    Tests for unify execplan functor

---
+++ core/src/test/kotlin/com.github.prologdb/execplan/UnifyFunctorTest.kt
@@ -0,0 +1,69 @@
+package com.github.prologdb.execplan
+
+import com.github.prologdb.async.buildLazySequence
+import com.github.prologdb.async.mapRemaining
+import com.github.prologdb.async.remainingToList
+import com.github.prologdb.dbms.DBProofSearchContext
+import com.github.prologdb.runtime.RandomVariableScope
+import com.github.prologdb.runtime.term.*
+import com.github.prologdb.runtime.unification.VariableBucket
+import io.kotlintest.matchers.shouldEqual
+import io.kotlintest.specs.FreeSpec
+import io.mockk.every
+import io.mockk.mockk
+import java.util.*
+
+class UnifyFunctorTest : FreeSpec({
+    val principal = UUID.randomUUID()
+    
+    "multiple inputs - all can be unified" {
+        val inputs = buildLazySequence(principal) {
+            yield(Pair(0L, Predicate("foo", arrayOf(PrologInteger(0)))))
+            yield(Pair(1L, Predicate("foo", arrayOf(PrologInteger(1)))))
+            yield(Pair(2L, Predicate("foo", arrayOf(PrologInteger(2)))))
+            yield(Pair(3L, Predicate("foo", arrayOf(PrologInteger(3)))))
+        }
+        val inputsWithVars = inputs.mapRemaining { Pair(VariableBucket(), it) }
+        val functor = UnifyFunctor(Predicate("foo", arrayOf(Variable("A"))))
+        val ctxt = mockk<DBProofSearchContext>()
+        every { ctxt.randomVariableScope } returns RandomVariableScope()
+        
+        val results = functor.invoke(ctxt, inputsWithVars).remainingToList()
+        
+        results shouldEqual listOf(
+            Pair(varsBucket("A" to PrologInteger(0)), 0L),
+            Pair(varsBucket("A" to PrologInteger(1)), 1L),
+            Pair(varsBucket("A" to PrologInteger(2)), 2L),
+            Pair(varsBucket("A" to PrologInteger(3)), 3L)
+        )
+    }
+
+    "multiple inputs - some do not unify" {
+        val inputs = buildLazySequence(principal) {
+            yield(Pair(0L, Predicate("foo", arrayOf(PrologInteger(0)))))
+            yield(Pair(1L, Predicate("foo", arrayOf(PrologInteger(1), Atom("x")))))
+            yield(Pair(2L, Predicate("foo", arrayOf(PrologInteger(2)))))
+            yield(Pair(3L, Predicate("foo", arrayOf(PrologInteger(3), Atom("y")))))
+        }
+        val inputsWithVars = inputs.mapRemaining { Pair(VariableBucket(), it) }
+        val functor = UnifyFunctor(Predicate("foo", arrayOf(Variable("A"))))
+        val ctxt = mockk<DBProofSearchContext>()
+        every { ctxt.randomVariableScope } returns RandomVariableScope()
+
+        val results = functor.invoke(ctxt, inputsWithVars).remainingToList()
+
+        results shouldEqual listOf(
+            Pair(varsBucket("A" to PrologInteger(0)), 0L),
+            Pair(varsBucket("A" to PrologInteger(2)), 2L)
+        )
+    }
+})
+
+private fun varsBucket(vararg of: Pair<String, Term>): VariableBucket {
+    val bucket = VariableBucket()
+    for ((varName, term) in of) {
+        bucket.instantiate(Variable(varName), term)
+    }
+    
+    return bucket
+}
