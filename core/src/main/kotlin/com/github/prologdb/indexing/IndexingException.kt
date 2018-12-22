aab4a08cc5d8d2096a2b44e32b6502cd4bc724d8
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-12-22 03:18:29 +0100

    indexing: added IndexingTemplate

--- core/src/main/kotlin/com/github/prologdb/indexing/IndexingException.kt
+++ core/src/main/kotlin/com/github/prologdb/indexing/IndexingException.kt
@@ -6,3 +6,8 @@ import com.github.prologdb.runtime.PrologException
  * Thrown on indexing errors
  */
 class IndexingException(message: String, cause: Throwable? = null) : PrologException(message, cause)
+
+/**
+ * Thrown when a supplied key cannot be used to search within or write to an index.
+ */
+class InvalidIndexKeyException(message: String, cause: Throwable? = null) : IndexingException(message, cause)
