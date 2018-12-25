2bcfc455bebba96199cd5454db683bbec31364a3
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-12-25 15:36:07 +0100

    indexing: first draft of FactIndexLoader interface

---
+++ core/src/main/kotlin/com/github/prologdb/indexing/FactIndexLoader.kt
@@ -0,0 +1,32 @@
+package com.github.prologdb.indexing
+
+import com.github.prologdb.dbms.DataDirectoryManager
+
+/**
+ * Creates [FactIndex]s for given [IndexDefinition]s. Each instance of [PersistentKnowledgeBase]
+ * has exactly one instance of this type; it is responsible for managing all indices in that
+ * knowledge base.
+ * 
+ * Implementations need not be thread-safe. Users should synchronize on the loader instance.
+ */
+interface FactIndexLoader {
+    /**
+     * Creates a new index with the given name:
+     * * sets up and allocates storage files
+     * * saves metadata about the index so that it can, after a restart, be discovered through
+     *   [open].
+     * @param directoryManager The location and scope of the index
+     */
+    fun create(directoryManager: DataDirectoryManager.ClauseStoreScope, definition: IndexDefinition): FactIndex
+
+    /**
+     * Opens and returns the index with the given [IndexDefinition.name]. The other properties
+     * of [IndexDefinition] are ignored.
+     * 
+     * @param directoryManager The location and scope of the index
+     * 
+     * @throws IndexNotFoundException If an index with 
+     */
+    @Throws(IndexNotFoundException::class)
+    fun open(directoryManager: DataDirectoryManager.ClauseStoreScope, definition: IndexDefinition): FactIndex
+}
--- core/src/main/kotlin/com/github/prologdb/indexing/IndexingException.kt
+++ core/src/main/kotlin/com/github/prologdb/indexing/IndexingException.kt
@@ -11,3 +11,8 @@ class IndexingException(message: String, cause: Throwable? = null) : PrologExcep
  * Thrown when a supplied key cannot be used to search within or write to an index.
  */
 class InvalidIndexKeyException(message: String, cause: Throwable? = null) : IndexingException(message, cause)
+
+/**
+ * Thrown when an index is referenced (e.g. by name) but such an index is not found.
+ */
+class IndexNotFoundException(message: String, cause: Throwable? = null): IndexingException(message, cause)
