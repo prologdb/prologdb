26d26535a57bfa3fadf8ae1ad01f35311cc4a217
Tobias Marstaller <tobias.marstaller@gmail.com>
2022-03-05 13:50:14 +0100

    client: fetch operators after every directive

--- client/src/main/kotlin/com/github/prologdb/client/frontend.kt
+++ client/src/main/kotlin/com/github/prologdb/client/frontend.kt
@@ -1,28 +1,38 @@
 package com.github.prologdb.client
 
 import com.github.prologdb.async.LazySequence
+import com.github.prologdb.async.forEachRemaining
+import com.github.prologdb.async.mapRemaining
 import com.github.prologdb.net.util.prettyPrint
 import com.github.prologdb.runtime.PrologException
+import com.github.prologdb.runtime.builtin.ISOOpsOperatorRegistry
+import com.github.prologdb.runtime.term.Atom
+import com.github.prologdb.runtime.term.PrologInteger
+import com.github.prologdb.runtime.term.Variable
 import com.github.prologdb.runtime.unification.Unification
+import com.github.prologdb.runtime.util.DefaultOperatorRegistry
+import com.github.prologdb.runtime.util.OperatorDefinition
+import com.github.prologdb.runtime.util.OperatorRegistry
+import com.github.prologdb.runtime.util.OperatorType
 
 class CLIFrontend(private val connection: Connection) {
-
-    private var mode = InstructionMode.QUERY
-
     fun run() {
+        var mode = InstructionMode.QUERY
+        var operators = ISOOpsOperatorRegistry
+
         repl@while (true) {
             print("${mode.operator} ")
             var instruction = (readLine() ?: "") .trim()
 
             var singleMode = mode
-            findMode@for (mode in InstructionMode.values()) {
-                if (instruction == mode.operator) {
-                    this.mode = mode
+            findMode@for (availableMode in InstructionMode.values()) {
+                if (instruction == availableMode.operator) {
+                    mode = availableMode
                     continue@repl
                 }
-                else if (instruction.startsWith(mode.operator)) {
-                    singleMode = mode
-                    instruction = instruction.substring(mode.operator.length).trim()
+                else if (instruction.startsWith(availableMode.operator)) {
+                    singleMode = availableMode
+                    instruction = instruction.substring(availableMode.operator.length).trim()
                     break@findMode
                 }
             }
@@ -60,7 +70,7 @@ class CLIFrontend(private val connection: Connection) {
                 print(
                     solution.variableValues.values.joinToString(
                         separator = " ,\n",
-                        transform = { "${it.first} = ${it.second}"}
+                        transform = { "${it.first} = ${it.second?.toStringUsingOperatorNotations(operators)}"}
                     )
                 )
                 if (solution.variableValues.isEmpty) {
@@ -79,6 +89,14 @@ class CLIFrontend(private val connection: Connection) {
             } while (solution != null)
 
             solutions.close()
+            if (singleMode == InstructionMode.DIRECTIVE) {
+                try {
+                    operators = fetchOperators()
+                }
+                catch (ex: GenericQueryError) {
+
+                }
+            }
         }
     }
 
@@ -87,6 +105,20 @@ class CLIFrontend(private val connection: Connection) {
             InstructionMode.QUERY     -> connection.startQuery(instruction)
             InstructionMode.DIRECTIVE -> connection.startDirective(instruction)
         }
+    }
+
+    private fun fetchOperators(): OperatorRegistry {
+        val operatorSolutions = init("current_op(Precedence, Associativity, Name)", InstructionMode.QUERY)
+        val registry = DefaultOperatorRegistry()
+        operatorSolutions
+            .mapRemaining { solution -> OperatorDefinition(
+                (solution.variableValues[Variable("Precedence")] as PrologInteger).value.toShort(),
+                OperatorType.valueOf((solution.variableValues[Variable("Associativity")] as Atom).name.uppercase()),
+                (solution.variableValues[Variable("Name")] as Atom).name
+            ) }
+            .forEachRemaining(registry::defineOperator)
+
+        return registry
     }
 }
 
