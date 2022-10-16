bf9e4d517976e70e7ff093cc71e31f7aba7a0fad
Tobias Marstaller <tobias.marstaller@beyonnex.io>
2022-10-16 18:43:43 +0200

    add TypedPredicateArguments.getQualifiedIndicator

--- core/src/main/kotlin/com/github/prologdb/dbms/builtin/meta/create_dynamic_predicate__2.kt
+++ core/src/main/kotlin/com/github/prologdb/dbms/builtin/meta/create_dynamic_predicate__2.kt
@@ -19,38 +19,21 @@ import java.util.UUID
 val BuiltinCreateDynamicPredicate2 = nativeDatabaseRule("create_dynamic_predicate", 2) { args, ctxt ->
     val runtime = ctxt.runtimeEnvironment as? MetaKnowledgeBaseRuntimeEnvironment
         ?: throw PrologInvocationContractViolationException("${args.indicator} must be invoked within a meta knowledge base.")
-
-    val inputs = FQI_INDICATOR_TEMPLATE.unify(args[0], ctxt.randomVariableScope)
-        ?.entries
-        ?.associate { it.first.name to it.second }
-        ?: throw ArgumentError(0, "must be a fully qualified indicator of this structure: $FQI_INDICATOR_TEMPLATE")
-
-    val moduleName = inputs["Module"] as? Atom
-        ?: throw ArgumentError(0, "must specify the module as an atom")
-    val functor = inputs["Functor"] as? Atom
-        ?: throw ArgumentError(0, "must specify the functor as an atom")
-    val arityTerm = inputs["Arity"] as? PrologNumber
-        ?: throw ArgumentError(0, "must specify the arity as an integer")
-    val arity = arityTerm
-        .takeIf { it > PrologNumber(0) && it <= PrologNumber(Int.MAX_VALUE) }
-        ?.toInteger()
-        ?.toInt()
-        ?: throw ArgumentError(0, "must specify an arity beteen 1 and ${Int.MAX_VALUE}")
+    val fqi = args.getQualifiedIndicator(0, ctxt)
     val options = ImplSelection.parse(args, 1)
 
-    val fqi = FullyQualifiedClauseIndicator(moduleName.name, ClauseIndicator.of(functor.name, arity))
     lateinit var uuid: UUID
 
     runtime.database.dataDirectory.modifySystemCatalog { systemCatalog ->
         val knowledgeBaseCatalog = systemCatalog.knowledgeBases.firstOrNull { it.name == runtime.knowledgeBaseCatalog.name }
             ?: throw KnowledgeBaseNotFoundException(runtime.knowledgeBaseCatalog.name)
-        val moduleCatalog = knowledgeBaseCatalog.modulesByName[moduleName.name]
+        val moduleCatalog = knowledgeBaseCatalog.modulesByName[fqi.moduleName]
         if (moduleCatalog == null) {
-            runtime.getLoadedModule(moduleName.name) // throws ModuleNotFoundException if it doesn't exist at all
-            throw ArgumentError(0, "module $moduleName is not a physical module.")
+            runtime.getLoadedModule(fqi.moduleName) // throws ModuleNotFoundException if it doesn't exist at all
+            throw ArgumentError(0, "module ${fqi.moduleName} is not a physical module.")
         }
 
-        if (moduleCatalog.predicates.any { it.functor == functor.name && it.arity == arity }) {
+        if (moduleCatalog.predicates.any { it.functor == fqi.indicator.functor && it.arity == fqi.indicator.arity }) {
             throw DynamicPredicateAlreadyExistsException(fqi)
         }
 
@@ -60,8 +43,8 @@ val BuiltinCreateDynamicPredicate2 = nativeDatabaseRule("create_dynamic_predicat
         systemCatalog.copy(knowledgeBases = (systemCatalog.knowledgeBases - knowledgeBaseCatalog) + knowledgeBaseCatalog.copy(
             modules = (knowledgeBaseCatalog.modules - moduleCatalog) + moduleCatalog.copy(
                 predicates = moduleCatalog.predicates + SystemCatalog.Predicate(
-                    functor = functor.name,
-                    arity = arity,
+                    functor = fqi.indicator.functor,
+                    arity = fqi.indicator.arity,
                     uuid = uuid,
                     factStoreImplementationId = null
                 )
@@ -76,12 +59,4 @@ val BuiltinCreateDynamicPredicate2 = nativeDatabaseRule("create_dynamic_predicat
 
     Unification.TRUE
 }
-
-private val FQI_INDICATOR_TEMPLATE = CompoundTerm(":", arrayOf(
-    Variable("Module"),
-    CompoundTerm("/", arrayOf(
-        Variable("Functor"),
-        Variable("Arity")
-    ))
-))
 
